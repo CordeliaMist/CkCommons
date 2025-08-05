647c72abd03098751736d6ffaf78e43e896c1a07
Cordelia Mist <buzzmodetrix@gmail.com>
2025-08-04 17:36:15 -0700

    add disabledTip method

--- CkGui/CkGui.Tips.cs
+++ CkGui/CkGui.Tips.cs
@@ -13,35 +13,48 @@ public static partial class CkGui
     public const string TipNL = "--NL--";
     public const string TipCol = "--COL--";
     private static readonly Regex TooltipTokenRegex = new($"({TipSep}|{TipNL}|{TipCol})", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
     /// <summary> A helper function to attach a tooltip to a section in the UI currently hovered. </summary>
     /// <remarks> If the string is null, empty, or whitespace, will do early return at no performance impact. </remarks>
-    public static void AttachToolTip(string? text, float borderSize = 1f, Vector4? color = null)
+    public static void AttachToolTip(string? text, Vector4? color = null)
     {
         if (string.IsNullOrWhiteSpace(text))
             return;
 
         // if the item is currently hovered, with the ImGuiHoveredFlags set to allow when disabled
         if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
-            ToolTipInternal(text, borderSize, color);
+            ToolTipInternal(text, color);
     }
 
     /// <summary> A helper function to attach a tooltip to a section in the UI currently hovered. </summary>
     /// <remarks> If the string is null, empty, or whitespace, will do early return at no performance impact. </remarks>
-    public static void AttachToolTipRect(Vector2 min, Vector2 max, string? text, float borderSize = 1f, Vector4? color = null)
+    public static void AttachToolTip(string? text, bool disabled, Vector4? color = null)
+    {
+        if (disabled || string.IsNullOrWhiteSpace(text))
+            return;
+
+        // if the item is currently hovered, with the ImGuiHoveredFlags set to allow when disabled
+        if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
+            ToolTipInternal(text, color);
+    }
+
+    /// <summary> A helper function to attach a tooltip to a section in the UI currently hovered. </summary>
+    /// <remarks> If the string is null, empty, or whitespace, will do early return at no performance impact. </remarks>
+    public static void AttachToolTipRect(Vector2 min, Vector2 max, string? text, Vector4? color = null)
     {
         if (string.IsNullOrWhiteSpace(text))
             return;
 
         // if the item is currently hovered, with the ImGuiHoveredFlags set to allow when disabled
         if (ImGui.IsMouseHoveringRect(min, max))
-            ToolTipInternal(text, borderSize, color);
+            ToolTipInternal(text, color);
     }
 
-    public static void ToolTipInternal(string text, float borderSize = 1f, Vector4? color = null)
+    public static void ToolTipInternal(string text, Vector4? color = null)
     {
         using var s = ImRaii.PushStyle(ImGuiStyleVar.WindowPadding, Vector2.One * 8f)
             .Push(ImGuiStyleVar.WindowRounding, 4f)
-            .Push(ImGuiStyleVar.PopupBorderSize, borderSize);
+            .Push(ImGuiStyleVar.PopupBorderSize, 1f);
         using var c = ImRaii.PushColor(ImGuiCol.Border, ImGuiColors.ParsedPink);
 
         ImGui.BeginTooltip();
